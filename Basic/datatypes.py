679c6e1a3739e17c3bc9f17283ec80bc0b94c0c8
KayodeOusi <kayodeolusi@gmail.com>
2022-01-11 03:00:41 +0100

    data types in python

---
+++ Basic/datatypes.py
@@ -0,0 +1,14 @@
+# Variables and data types
+
+owners_name = "John"
+owners_age = "50"
+
+# using concatenation (can only concatenate string to string)
+print("The owner's name is " + owners_name)
+print("He is " + owners_age + " years old")
+
+
+# data types
+my_name = "Kayode"     # string 
+my_age = 59            # number
+is_male = True         # boolean
