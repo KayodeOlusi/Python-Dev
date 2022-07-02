d756ee2db102661a039fd27afa126b8d29844bfd
KayodeOusi <kayodeolusi@gmail.com>
2022-07-02 23:53:31 +0100

    dictionaries

---
+++ dictionary/dictionary.py
@@ -0,0 +1,19 @@
+# dictionaries
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConversions.get("Jan")) # January
+print(monthConversions.get("Abc", "Not a valid key")) # To set a default value for a key that does not exist
