b9c4d1f64e10decb86ac3ec8e27ab346c88a1586
Kayode Oluwausi <66219016+KayodeOlusi@users.noreply.github.com>
2022-07-11 23:26:42 +0100

    ...

--- Lists/lists.py
+++ Lists/lists.py
@@ -4,8 +4,8 @@ friends = ["Kavin", "Ben", "Mike", "Mike"] # store the values in a list inside [
 print(friends) # return the list
 print(friends[0]) # return the first value in the list
 print(friends[-2]) # returns the value in the index starting from the last value
-print(friends[1 : 3]) # returns a range of values
-print(friends[1 : ]) # returns the second value and the rest after it
+print(friends[1:3]) # returns a range of values
+print(friends[1:]) # returns the second value and the rest after it
 
 
 numbers = [4, 5, 9, 2, 10, 22, 16]
