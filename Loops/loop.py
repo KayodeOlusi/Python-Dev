9d7297e9e76951a48229be326704d1242fb40c50
KayodeOusi <kayodeolusi@gmail.com>
2022-07-13 20:08:36 +0100

    exponentials

--- Loops/loop.py
+++ Loops/loop.py
@@ -1,9 +1,19 @@
-number = 0
-friends = ["Jim", "Karen", "Kevin"]
+# number = 0
+# friends = ["Jim", "Karen", "Kevin"]
 
-for index in range(len(friends)):
-    print(friends[index])
+# for index in range(len(friends)):
+#     print(friends[index])
     
 # while number < 50:
 #     print(number)
 #     number += 2
+
+def raise_to_power(base_num, pow_num):
+    result = 1
+    count = 1
+    while count <= pow_num:
+        result = result * base_num
+        count += 1
+    return result
+
+print(raise_to_power(2, 3))
